17b567e5faaf0e788e8429cb344c45f77d441ee2
GregSchy <105386198+GregSchy@users.noreply.github.com>
2022-11-02 14:54:37 +0100

    Do naprawy fireabase

--- firebase.js
+++ firebase.js
@@ -1,10 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBao0_Z2TQnyAnHm7LCp1lpDWST0CI9-K0",
-    authDomain: "amazn-clone-react.firebaseapp.com",
-    projectId: "amazn-clone-react",
-    storageBucket: "amazn-clone-react.appspot.com",
-    messagingSenderId: "702234903889",
-    appId: "1:702234903889:web:2f813764cf65ce32707f70"
-  };
+  apiKey: "AIzaSyBao0_Z2TQnyAnHm7LCp1lpDWST0CI9-K0",
+  authDomain: "amazn-clone-react.firebaseapp.com",
+  projectId: "amazn-clone-react",
+  storageBucket: "amazn-clone-react.appspot.com",
+  messagingSenderId: "702234903889",
+  appId: "1:702234903889:web:2f813764cf65ce32707f70"
+};
+
+
+  
